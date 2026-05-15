2ef8efc5d170641e9489b6d6fa0277a99559181a
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-15 16:05:18 +0800

    Add Student class with inherited personal information and academic attributes
    Extended the Person class by creating a Student class that inherits full name functionality while introducing student-specific details such as student ID, course name, year level, and general average. This update strengthens the structure of the student management system by organizing academic data in a more object-oriented approach.

--- smart_student_management_system.py
+++ smart_student_management_system.py
@@ -12,3 +12,19 @@ class Person:
 
     def display_information(self):
         print(f"Full Name: {self._full_name}")
+
+class Student(Person):
+    def __init__(
+        self,
+        student_id,
+        full_name,
+        course_name,
+        year_level,
+        general_average
+    ):
+        super().__init__(full_name)
+
+        self.__student_id = student_id
+        self.__course_name = course_name
+        self.__year_level = year_level
+        self.__general_average = general_average

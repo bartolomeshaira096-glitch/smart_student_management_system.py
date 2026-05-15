c8d8a047bcf1f93e21a42232f86508b1e10f35bd
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-05-16 01:40:21 +0800

    Implement Person class with encapsulation for full name

--- person.py
+++ person.py
@@ -1,12 +1,9 @@
 class Person:
-    def __init__(self, full_name):
-        self._full_name = full_name
+    def __init__(self, name):
+        self._full_name = name
 
     def get_full_name(self):
         return self._full_name
 
-    def set_full_name(self, updated_full_name):
-        self._full_name = updated_full_name
-
-    def display_information(self):
-        print(f"Full Name: {self._full_name}")
+    def set_full_name(self, name):
+        self._full_name = name
